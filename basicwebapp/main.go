6c24d99985e6e9ac52661f5e32ab53267796327c
Gergely Brautigam <gbrautigam@chemaxon.com>
2015-08-05 16:46:37 +0200

    Heroku support for this app.

--- basicwebapp/main.go
+++ basicwebapp/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/russross/blackfriday"
 )
 
 func main() {
+	//Port is provided by heroku which we need to bind to.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8484"
+	}
+
 	http.HandleFunc("/markdown", generateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("assets")))
-	http.ListenAndServe(":8484", nil)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func generateMarkdown(rw http.ResponseWriter, r *http.Request) {
