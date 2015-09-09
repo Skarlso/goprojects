ae1cabfeeea66f6adf21170bf54f7b806a94493e
Gergely Brautigam <gbrautigam@chemaxon.com>
2015-09-09 16:43:35 +0200

    Added a handler and test data to serve up a wiki page.

--- golangwiki/wiki.go
+++ golangwiki/wiki.go
@@ -1,6 +1,10 @@
 package main
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 //Page represents a Page
 type Page struct {
@@ -8,11 +12,31 @@ type Page struct {
 	Body  []byte
 }
 
+func main() {
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is some test content.")}
+	// p1.save()
+	// p2, _ := loadPage(p1.Title)
+	// fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8989", nil)
+}
+
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func (p *Page) load(title string) *Page {
-	file
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
