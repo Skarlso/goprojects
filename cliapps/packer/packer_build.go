5c033ea58e7096d783fd4b757bf6111e494bf818
Gergely Brautigam <gbrautigam@chemaxon.com>
2015-11-11 13:08:26 +0100

    Added the ability to pass in some number of arguments to build, for example -force.

--- cliapps/packer/packer_build.go
+++ cliapps/packer/packer_build.go
@@ -12,24 +12,36 @@ func main() {
 	if len(os.Args[1:]) == 0 {
 		log.Fatal("Please provide a templateFile.")
 	}
-
 	templateFile = os.Args[1]
+
+	argumentList := []string{"build"}
+	if len(os.Args[1:]) > 1 {
+		argumentList = append(argumentList, os.Args[2:]...)
+	}
+	argumentList = append(argumentList, templateFile)
 
 	if _, err := os.Stat(templateFile); err != nil {
 		log.Fatalf("Error using template file: %s, %v", templateFile, err)
 	}
+
+	checkIfScriptsExistInTemplate()
 
 	validate := exec.Command("packer", "validate", templateFile)
 	err := validate.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Validate was a success. Starting packer build, and not waiting for it to finish.")
-	packer := exec.Command("packer", "build", templateFile)
+
+	log.Println("Validate was a success. Starting packer with arguments:", argumentList)
+	packer := exec.Command("packer", argumentList...)
 	packer.Stdout = os.Stdout
 	packer.Stderr = os.Stderr
 	err = packer.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func checkIfScriptsExistInTemplate() {
+
 }
