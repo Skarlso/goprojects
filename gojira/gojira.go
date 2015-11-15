0bed8d1d09b733c5fbf4fc294627b064ef57944b
Gergely Brautigam <skarlso777@gmail.com>
2015-11-15 22:38:58 +0100

    Added a config toml.

--- gojira/gojira.go
+++ gojira/gojira.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+var configFile = "jira_config.toml"
+
+//Issue a representation of a JIRA Issue
+type Issue struct {
+	name        string
+	issueNumber string
+	priority    string
+}
+
+//Credentials a representation of a JIRA config which helds API permissions
+type Credentials struct {
+	Username string
+	Password string
+}
+
+func initConfig() Credentials {
+	if _, err := os.Stat(configFile); err != nil {
+		log.Fatalf("Error using config file: %v", err)
+	}
+
+	var cred Credentials
+
+	if _, err := toml.DecodeFile(configFile, &cred); err != nil {
+		log.Fatal("Error during decoding toml config: ", err)
+	}
+
+	return cred
+}
 
 func main() {
+	cred := initConfig()
 	fmt.Println("Usage information")
+	fmt.Printf("Username: %s, Password: %s\n", cred.Username, cred.Password)
 }
