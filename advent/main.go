141aef11abb828f9edd10875c8679d95223bd218
Gergely Brautigam <skarlso777@gmail.com>
2015-12-18 13:34:09 +0100

    Added solution for day 3.

--- advent/main.go
+++ advent/main.go
@@ -3,6 +3,7 @@ package main
 import "github.com/skarlso/goprojects/advent/solutions"
 
 func main() {
-	solutions.MoveSanta()
-	solutions.WrappingPaper()
+	// solutions.MoveSanta()
+	// solutions.WrappingPaper()
+	solutions.DeliverPresents()
 }
--- advent/solutions/presents.go
+++ advent/solutions/presents.go
@@ -1,5 +1,61 @@
 package solutions
 
-func DeliverPresents() {
+import (
+	"fmt"
+	"io/ioutil"
+)
 
+//location of a house
+type location struct {
+	x, y int
+}
+
+//DeliverPresents counts how many houses got presents this year
+func DeliverPresents() {
+	//Read in file input
+	fileContent, err := ioutil.ReadFile("solutions/present_input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	//Playing field
+	field := make(map[location]bool, 0)
+
+	//First House init location
+	l := location{0, 0}
+
+	//Get a new field with the first house as starting point which got visited
+	field[l] = true
+
+	//Begin from 0 and then move around
+	newX := 0
+	newY := 0
+
+	//Moving Santa
+	for _, v := range fileContent {
+
+		switch v {
+		case '^':
+			newY++
+		case 'v':
+			newY--
+		case '<':
+			newX--
+		case '>':
+			newX++
+		}
+
+		newLocation := location{newX, newY}
+		field[newLocation] = true
+	}
+
+	//All the houses which got visited will be true
+	var housesVisited int
+	for _, v := range field {
+		if v {
+			housesVisited++
+		}
+	}
+
+	fmt.Println(housesVisited)
 }
