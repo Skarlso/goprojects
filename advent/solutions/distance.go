d79d52af988bd083b9832f7b231db268430b4547
Gergely Brautigam <skarlso777@gmail.com>
2015-12-23 07:11:05 +0100

    In small incremental steps

--- advent/solutions/distance.go
+++ advent/solutions/distance.go
@@ -28,5 +28,30 @@ func CalculateDistance() {
 		connections[split[0]] = append(connections[split[0]], Targets{split[2], distance})
 	}
 
-	fmt.Println(connections)
+	fmt.Println("Distance for AlphaCentauri and Tambi", getPathDistanceBetweenPoints([]string{"AlphaCentauri", "Tambi"}))
+	generatePossibleRoutes()
+}
+
+//Retrieves distances for a route
+//Route is represented by a sorted list
+func getPathDistanceBetweenPoints(loc []string) int {
+	dis := connections[loc[0]][0].distance
+	return dis
+}
+
+//Generates possibles routes between locations which are connected
+func generatePossibleRoutes() {
+	//Lookup a location and then lookup that locations locations and connect it.
+	//And then lookup that locations locations locations..... Recursive? Binary tree?
+	//Begin small -> Locations for AlphaCentauri
+	fmt.Println("Locations for AlphaCentauri:", connections["AlphaCentauri"])
+	var possibleRoute []string
+	possibleRoute = append(possibleRoute, "AlphaCentauri")
+	for _, v := range connections["AlphaCentauri"] {
+		fmt.Printf("Inner locations for: %s:%v\n", v.name, connections[v.name])
+		if r, ok := connections[v.name]; ok {
+			possibleRoute = append(possibleRoute, r[0].name)
+		}
+	}
+	fmt.Println("Possible route combination:", possibleRoute)
 }
