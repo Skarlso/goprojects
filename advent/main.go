cd760df35ddbed4256b634b71325ce591c226ada
Gergely Brautigam <skarlso777@gmail.com>
2015-12-28 16:17:18 +0100

    Added a simple permutation

--- advent/main.go
+++ advent/main.go
@@ -1,15 +1,14 @@
 package main
 
-import "github.com/skarlso/goprojects/advent/solutions"
-
-func main() {
-	// solutions.MoveSanta()
-	// solutions.WrappingPaper()
-	// solutions.DeliverPresents()
-	// solutions.Mine()
-	// solutions.NiceOrNaughty()
-	// solutions.TurnOnTheLightsV2()
-	// solutions.BobbyTable()
-	// solutions.EscapeV2()
-	solutions.CalculateDistance()
-}
+//
+// func main() {
+// 	// solutions.MoveSanta()
+// 	// solutions.WrappingPaper()
+// 	// solutions.DeliverPresents()
+// 	// solutions.Mine()
+// 	// solutions.NiceOrNaughty()
+// 	// solutions.TurnOnTheLightsV2()
+// 	// solutions.BobbyTable()
+// 	// solutions.EscapeV2()
+// 	solutions.CalculateDistance()
+// }
--- exercise/permutation.go
+++ exercise/permutation.go
@@ -1,7 +1,20 @@
 package main
 
+import "fmt"
+
 var locations = []string{"London", "Dublin", "Belfast"}
 
+func permute(s []string, n int) {
+	if n == 1 {
+		fmt.Println(s)
+	}
+	for i := 0; i < n; i++ {
+		s[i], s[n-1] = s[n-1], s[i]
+		permute(s, n-1)
+		s[i], s[n-1] = s[n-1], s[i]
+	}
+}
+
 func main() {
-    for i := 0
+	permute(locations, len(locations))
 }
