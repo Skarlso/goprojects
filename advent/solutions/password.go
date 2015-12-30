baac755434060687d0d43f779ca2c9ce0693e931
Gergely Brautigam <skarlso777@gmail.com>
2015-12-30 13:22:26 +0100

    Almost there. Just need to adjust how the increment is working.

--- advent/solutions/password.go
+++ advent/solutions/password.go
@@ -1,6 +1,9 @@
 package solutions
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 /*
 * 1. Increment a letter
@@ -13,26 +16,69 @@ import "fmt"
 var passwordInput = []byte("hxbxwxba")
 
 //GenerateNewPassword generates a new password for Santa
-func GenerateNewPassword(starting string) (passwd string) {
-	return
+func GenerateNewPassword() {
+	fmt.Println(string(incrementalPasswordGenerate(passwordInput)))
 }
 
-func checkCorrectness(s []byte) bool {
-	fmt.Println("Checking correctness of:", string(s))
-	if len(s) == 8 {
-		return true
+func checkIncreasingTriplet(s []byte) bool {
+	fmt.Println("Checking for Increasing triplets in:", string(s))
+	for i := range s {
+		if i+2 < len(s) {
+			if s[i]+1 == s[i+1] && s[i]+2 == s[i+2] {
+				return true
+			}
+		}
 	}
 	return false
 }
 
-func incrementPassword(in []byte) {
+func checkCorrectness(s []byte) bool {
+	return checkForbiddenLetters(string(s)) && checkIncreasingTriplet(s) && checkNonOverlappingDifferentPairs(s)
+}
+
+func checkForbiddenLetters(s string) bool {
+	fmt.Println("Checking for forbidden letters in:", s)
+	return strings.ContainsAny(s, "i & o & l")
+}
+
+func checkNonOverlappingDifferentPairs(s []byte) bool {
+	fmt.Println("Checking for Non Overlapping Pairs in:", string(s))
+	pairCount := 0
+	skipCount := 0
+	skip := false
+	for i := range s {
+		if skip {
+			skipCount++
+			if skipCount == 2 {
+				skip = false
+			}
+			continue
+		}
+
+		if i+1 < len(s) {
+			if s[i] == s[i+1] {
+				pairCount++
+				skip = true
+			}
+		}
+	}
+
+	return pairCount > 1
+}
+
+func incrementalPasswordGenerate(in []byte) []byte {
 	for i := len(in) - 1; i >= 0; i-- {
-		for in[i] != 'a' {
+		origin := in[i]
+		in[i]++
+		for in[i] != origin {
 			in[i] -= 'a'
 			in[i] = (in[i] + 1) % ('z' - 'a')
 			in[i] += 'a'
-			checkCorrectness(in)
+			if checkCorrectness(in) {
+				return in
+			}
 		}
 	}
-	fmt.Println(string(in))
+
+	return nil
 }
--- advent/solutions/password_test.go
+++ advent/solutions/password_test.go
@@ -1,7 +1,65 @@
 package solutions
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIncrementingPasswordCharacterByCharacter(*testing.T) {
 	incrementPassword(passwordInput)
+}
+
+func TestTriplet(t *testing.T) {
+	sample := "aaabcaa"
+	if !checkIncreasingTriplet([]byte(sample)) {
+		t.Error("Triple was not found in:", sample)
+	} else {
+		t.Log("Triplet found in sample:", sample)
+		fmt.Println("Triplet Found in sample:", sample)
+	}
+
+	errorSample := "abddd"
+	if !checkIncreasingTriplet([]byte(errorSample)) {
+		fmt.Println("Triplet not found.")
+		t.Log("Triple was not found in:", errorSample)
+	} else {
+		t.Error("Triplet found in sample:", errorSample)
+		fmt.Println("Triplet Found in sample:", errorSample)
+	}
+}
+
+func TestForbiddenLetters(t *testing.T) {
+	sample := "forbidden"
+
+	if checkForbiddenLetters(sample) {
+		fmt.Println("Found forbidden letters which is correct.")
+	} else {
+		t.Error("Did not find forbidden letters.")
+	}
+
+	errorSample := "abdsc"
+
+	if checkForbiddenLetters(errorSample) {
+		t.Error("Should have returned false for: ", errorSample)
+	} else {
+		fmt.Println("All Good.")
+	}
+}
+
+func TestNonOverlappingPair(t *testing.T) {
+	sample := "aacbb"
+
+	if checkNonOverlappingDifferentPairs([]byte(sample)) {
+		fmt.Println("All Good.")
+	} else {
+		t.Error("Did not find overlapping pairs in:", sample)
+	}
+
+	errorSample := "aabb"
+
+	if checkNonOverlappingDifferentPairs([]byte(errorSample)) {
+		t.Error("Found overlapping pairs in:", errorSample)
+	} else {
+		fmt.Println("All Good.")
+	}
 }
