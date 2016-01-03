a4d8e5eeb63268b7feba90aeee420f8d5cbaaeaa
Gergely Brautigam <skarlso777@gmail.com>
2016-01-03 10:23:00 +0100

    Worked on it a bit...

--- advent/day12/part1/accounting.go
+++ advent/day12/part1/accounting.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 )
 
 func main() {
-	fileContent, err := ioutil.ReadFile("input.txt")
+	//420
+	fileContent, err := ioutil.ReadFile("smaller_input.txt")
 	if err != nil {
 		panic(err)
 	}
+	// sum := 0
+	var jsonMessage interface{}
 
-	fmt.Println(fileContent)
+	err = json.Unmarshal(fileContent, &jsonMessage)
+	if err != nil {
+		panic(err)
+	}
+	m := jsonMessage.(map[string]interface{})
+	fmt.Println(m)
+	//Recursive travel through the tree of maps.
 }
