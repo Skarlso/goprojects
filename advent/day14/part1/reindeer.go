4017f60920c34dccd02f932dd5cd32c0b74470f1
Skarlso <skarlso777@gmail.com>
2016-01-06 13:10:54 +0100

    Added filling up the reindeers. Though I need to check out Fscanf better.

--- advent/day14/part1/reindeer.go
+++ advent/day14/part1/reindeer.go
@@ -1,4 +1,48 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+//Reindeer this is a reindeer, has a name, a speed and a time it requires to rest
+type Reindeer struct {
+	name  string
+	speed int
+	limit int
+	sleep int
+}
+
+var reindeers []Reindeer = make([]Reindeer, 0)
+
+func init() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	in := bufio.NewReader(file)
+	for {
+		var (
+			name                string
+			speed, limit, sleep int
+		)
+		v, err := fmt.Fscanf(in, "%s can fly %d km/s for %d seconds, but then must rest for %d seconds.", &name, &speed, &limit, &sleep)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+		if v == 0 {
+			continue
+		}
+		r := Reindeer{name, speed, limit, sleep}
+		reindeers = append(reindeers, r)
+	}
+}
+
 func main() {
+	fmt.Println("Reindeers:", reindeers)
+}
+
+func startRace() {
 }
