a2cb582a03222ae40d64971dd0b60cafecab1263
Gergely Brautigam <skarlso777@gmail.com>
2016-01-10 08:41:48 +0100

    Working on it...

--- advent/day18/part1/lights.go
+++ advent/day18/part1/lights.go
@@ -5,6 +5,11 @@ import (
 	"io/ioutil"
 )
 
+//Location defines coordinates on a grid
+type Location struct {
+	x, y int
+}
+
 const (
 	//GRIDX Maximum grid dimension X
 	GRIDX = 100
@@ -14,9 +19,23 @@ const (
 	ON = 1
 	//OFF Defines a light which is off
 	OFF = 0
+	//LIMIT defines maximum iteration count on grid
+	LIMIT = 100
 )
 
-var lightGrid = make([][]int, GRIDX)
+var (
+	lightGrid = make([][]bool, GRIDX)
+	corners   = []Location{
+		{x: -1, y: -1},
+		{x: 0, y: -1},
+		{x: 1, y: -1},
+		{x: 1, y: 0},
+		{x: 1, y: 1},
+		{x: 0, y: 1},
+		{x: -1, y: 1},
+		{x: -1, y: 0},
+	}
+)
 
 func init() {
 	fileContent, err := ioutil.ReadFile("input.txt")
@@ -25,14 +44,13 @@ func init() {
 	}
 
 	for i := range lightGrid {
-		lightGrid[i] = make([]int, GRIDY)
+		lightGrid[i] = make([]bool, GRIDY)
 	}
-
 	x := 0
 	y := 0
 	for _, v := range fileContent {
 		if v == '#' {
-			lightGrid[x][y] = 1
+			lightGrid[x][y] = true
 		}
 		y = y + 1
 		if v == '\n' {
@@ -43,6 +61,61 @@ func init() {
 
 }
 
+//animate Animate the lights, Conway's Game of Life style
+func animate() {
+	for i := 0; i < LIMIT; i++ {
+		for x := 0; x < GRIDX; x++ {
+			for y := 0; y < GRIDY; y++ {
+				animateLightAt(x, y)
+			}
+		}
+	}
+}
+
+func animateLightAt(x, y int) {
+	onCount := 0
+	currentLight := lightGrid[x][y]
+	for _, v := range corners {
+		newX := x + v.x
+		newY := y + v.y
+		if (newX >= 0 && newX < GRIDX) && (newY >= 0 && newY < GRIDY) {
+			if lightGrid[newX][newY] {
+				onCount++
+			}
+		}
+	}
+	// fmt.Println("On Count:", onCount)
+	if onCount == 3 && !currentLight {
+		lightGrid[x][y] = true
+	}
+
+	if (onCount != 2 && onCount != 3) && currentLight {
+		lightGrid[x][y] = false
+	}
+
+	// I leave this here because I liked it, but realized that wasn't that easy
+	// if onCount >= 2 && onCount <= 3 {
+	// 	lightGrid[x][y] &^= 1
+	// }
+	//
+	// if onCount == 3 {
+	// 	lightGrid[x][y] |= 1
+	// }
+}
+
+func countOnLights() (count int) {
+	for i := 0; i < GRIDX; i++ {
+		for j := 0; j < GRIDY; j++ {
+			if lightGrid[i][j] {
+				count++
+			}
+		}
+	}
+
+	return
+}
+
 func main() {
-	fmt.Println("Grid Is Loaded:", lightGrid)
+	animate()
+	fmt.Println("Lights which are on:", countOnLights())
 }
