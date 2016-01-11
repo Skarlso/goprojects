fa5462262cd06461d1fafd9e0b036dabf548df26
Gergely Brautigam <skarlso777@gmail.com>
2016-01-11 07:13:18 +0100

    Initial lazy commit.

---
+++ advent/day19/part1/molecules.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/skarlso/goutils/arrayutils"
+)
+
+var molecule = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"
+var combinations []string
+var replacements = make(map[string]string)
+
+func init() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		var mol string
+		var replace string
+		split := strings.Split(line, "=>")
+		if len(split) > 1 {
+			fmt.Println(split)
+			mol = strings.Trim(split[0], " ")
+			replace = strings.Trim(split[1], " ")
+			replacements[mol] = replace
+		}
+	}
+}
+
+func replace() {
+	for i, v := range molecule {
+		split := strings.Split(molecule, "")
+		split[i] = replacements[string(v)]
+		newComb := strings.Join(split, "")
+		if !arrayutils.ContainsString(combinations, newComb) {
+			combinations = append(combinations, newComb)
+		}
+	}
+}
+
+func main() {
+	replace()
+	fmt.Println("Maximum number of combinations:", len(combinations))
+}
