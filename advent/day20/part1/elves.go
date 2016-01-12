e449115b7a1f478c3621466452e0ce69ab13f634
Skarlso <skarlso777@gmail.com>
2016-01-12 10:50:28 +0100

    Counting dividents was not fast enough either. Than I realized there is a much simpler solution to it.

--- advent/day20/part1/elves.go
+++ advent/day20/part1/elves.go
@@ -1,55 +1,24 @@
 package main
 
+import "fmt"
+
 const (
 	//LIMIT is the limit for a house's present. More than enough presents...
 	LIMIT = 34000000
 )
 
-var houses = make(map[int]int, 0)
-var elves = make([]Elf, 0)
-
-//Elf represents either a pointed eared wise a** or a little fellow delivering presents
-type Elf struct {
-	elfNumber int
-}
-
 func main() {
-	houseNumber := 1
-	for {
-		//houseNumber and all of it's dividents * 10.
-		// 6's dividents -> 6, 3, 2, 1 -> 6 + 3 + 2 + 1 -> 6 + 6 -> 12
-		houseNumber++
+	houses := make([]int, LIMIT/10)
+	for i := 1; i < LIMIT/10; i++ {
+		for j := i; j < LIMIT/10; j += i {
+			houses[j] += i * 10
+		}
+	}
+
+	for i, v := range houses {
+		if v >= LIMIT {
+			fmt.Println(i)
+			break
+		}
 	}
 }
-
-// func main() {
-// 	houseNumber := 1
-// 	done := make(chan bool)
-// 	for {
-// 		houses[houseNumber] = 0
-// 		go visitHouses(houseNumber, done)
-// 		houseNumber++
-// 		if <-done {
-// 			break
-// 		}
-// 	}
-// 	// fmt.Println("houses:", houses)
-// 	// fmt.Println("elves:", elves)
-//
-// }
-//
-// func visitHouses(houseNumber int, done chan bool) {
-// 	newElf := Elf{houseNumber}
-// 	elves = append(elves, newElf)
-// 	for _, e := range elves {
-// 		if houseNumber%e.elfNumber == 0 {
-// 			houses[houseNumber] += e.elfNumber * 10
-// 			// fmt.Println("House number and value:", houseNumber, houses[houseNumber])
-// 			if houses[houseNumber] >= LIMIT {
-// 				fmt.Println("Found house with limit: ", houseNumber)
-// 				done <- true
-// 			}
-// 		}
-// 	}
-// 	done <- false
-// }
