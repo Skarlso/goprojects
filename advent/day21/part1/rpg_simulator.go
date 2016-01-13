3af5bbc5dbe62f6d84a4fc4fb8ea12ad4b3b03e5
Skarlso <skarlso777@gmail.com>
2016-01-13 09:52:11 +0100

    Done with part 1.

--- advent/day21/part1/rpg_simulator.go
+++ advent/day21/part1/rpg_simulator.go
@@ -1,25 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //Boss The Baus
 type Boss struct {
 	hp, dmg, armor int
 }
 
-//Inventory the inventory of the player. Items are represented by numbers only
-//since he can have only one of each, except for rings. If ring is -1 it's armor
-//if ring is +1 it's damage (gone with separate rings for now. Later I can optimize.)
-type Inventory struct {
-	weapond, armor int
-	defRings       []int
-	dmgRings       []int
-}
-
 //Player The Playa
 type Player struct {
-	Inventory
-	hp, dmg, armor, moneySpent int
+	hp, dmg, armor int
 }
 
 //Weapon weapon's representation with damage and cost
@@ -52,98 +45,124 @@ type DamageRing struct {
 
 //Shop a shop which has a variaty of items
 type Shop struct {
-	weapons      []Weapon
-	armors       []Armor
-	defenseRings []DefenseRing
-	damageRings  []DamageRing
+	weapons      map[int]Weapon
+	armors       map[int]Armor
+	defenseRings map[int]DefenseRing
+	damageRings  map[int]DamageRing
 }
 
-var player *Player
-var boss *Boss
 var shop Shop
 var itemCombinations []int
 
 func init() {
 	shop = Shop{
-		weapons: []Weapon{
-			Weapon{"Dagger", 4, 8},
-			Weapon{"Shortsword", 10, 5},
-			Weapon{"Warhammer", 25, 6},
-			Weapon{"Longsword", 40, 7},
-			Weapon{"Greataxe", 74, 8},
+		weapons: map[int]Weapon{
+			0: {"Dagger", 8, 4},
+			1: {"Shortsword", 10, 5},
+			2: {"Warhammer", 25, 6},
+			3: {"Longsword", 40, 7},
+			4: {"Greataxe", 74, 8},
 		},
-		armors: []Armor{
-			Armor{"Leather", 13, 1},
-			Armor{"Chainmail", 31, 2},
-			Armor{"Splintmail", 53, 3},
-			Armor{"Bandedmail", 75, 4},
-			Armor{"Platemail", 102, 5},
+		armors: map[int]Armor{
+			1: {"Leather", 13, 1},
+			2: {"Chainmail", 31, 2},
+			3: {"Splintmail", 53, 3},
+			4: {"Bandedmail", 75, 4},
+			5: {"Platemail", 102, 5},
 		},
-		defenseRings: []DefenseRing{
-			DefenseRing{"Defense +1", 20, 1},
-			DefenseRing{"Defense +2", 40, 2},
-			DefenseRing{"Defense +3", 80, 3},
+		defenseRings: map[int]DefenseRing{
+			1: {"Defense +1", 20, 1},
+			2: {"Defense +2", 40, 2},
+			3: {"Defense +3", 80, 3},
 		},
-		damageRings: []DamageRing{
-			DamageRing{"Damage +1", 25, 1},
-			DamageRing{"Damage +2", 50, 2},
-			DamageRing{"Damage +3", 100, 3},
+		damageRings: map[int]DamageRing{
+			1: {"Damage +1", 25, 1},
+			2: {"Damage +2", 50, 2},
+			3: {"Damage +3", 100, 3},
 		},
 	}
-}
-
-func setupGame() {
-	player = &Player{hp: 100, dmg: 0, armor: 0, moneySpent: 0, Inventory: Inventory{}}
-	boss = &Boss{hp: 103, dmg: 9, armor: 2}
-}
-
-func getWeaponsCombinations() {
-	// weaponCombinations := itertools.Combinations(shop.weapons, len(shop.weapons))
-}
-
-func getArmorCombinations() {
-
-}
-
-func getDefenseRingCombinations() {
-
-}
-
-func getAttackRingCombinations() {
-
 }
 
 func main() {
-	playerWon := false
-	for !playerWon {
-		setupGame()
-		player.buyItems()
-		playersTurn := true
-		for player.hp > 0 && boss.hp > 0 {
-			fmt.Printf("Player's hp:%d | Boss hp:%d \n", player.hp, boss.hp)
-			switch playersTurn {
-			case true:
-				player.attack(boss)
-				playersTurn = false
-			case false:
-				boss.attack(player)
-				playersTurn = true
+	smallestCost := math.MaxInt64
+	var (
+		weapondmg int
+		armor     int
+		defring   int
+		dmgring   int
+		cWeapond  int
+		cArmor    int
+		cDefRing  int
+		cDmgRing  int
+	)
+
+	for _, v := range shop.weapons {
+		fmt.Println("Starting game with weapond:", v.name)
+		weapondmg = v.damage
+		cWeapond = v.cost
+		for a := 0; a <= len(shop.armors); a++ {
+			if _, ok := shop.armors[a]; ok {
+				fmt.Println("Starting game with armor:", shop.armors[a].name)
+				armor = shop.armors[a].armor
+				cArmor = shop.armors[a].cost
+			} else {
+				armor = 0
+				cArmor = 0
+			}
+			for defr := 0; defr <= len(shop.defenseRings); defr++ {
+				if _, ok := shop.defenseRings[defr]; ok {
+					fmt.Println("Starting game with defense ring:", shop.defenseRings[defr].name)
+					defring = shop.defenseRings[defr].defense
+					cDefRing = shop.defenseRings[defr].cost
+				} else {
+					defring = 0
+					cDefRing = 0
+				}
+				for dmgr := 0; dmgr <= len(shop.damageRings); dmgr++ {
+					if _, ok := shop.damageRings[dmgr]; ok {
+						fmt.Println("Starting game with damage ring:", shop.damageRings[dmgr].name)
+						dmgring = shop.damageRings[dmgr].damage
+						cDmgRing = shop.damageRings[dmgr].cost
+					} else {
+						dmgring = 0
+						cDmgRing = 0
+					}
+					moneySpent := cWeapond + cArmor + cDefRing + cDmgRing
+					playersTurn := true
+
+					player := &Player{hp: 100, dmg: weapondmg + dmgring, armor: armor + defring}
+					boss := &Boss{hp: 103, dmg: 9, armor: 2}
+					fmt.Println("Player:", *player)
+					fmt.Println("Boss:", *boss)
+					for {
+						// fmt.Printf("Player's hp:%d | Boss hp:%d \n", player.hp, boss.hp)
+						switch playersTurn {
+						case true:
+							player.attack(boss)
+							playersTurn = false
+						case false:
+							boss.attack(player)
+							playersTurn = true
+						}
+
+						if player.hp <= 0 || boss.hp <= 0 {
+							break
+						}
+					}
+
+					if player.hp > 0 {
+						if moneySpent < smallestCost {
+							smallestCost = moneySpent
+						}
+					}
+
+				}
 			}
 		}
 
-		if player.hp > 0 {
-			playerWon = true
-		}
+		fmt.Println("Smallest cost spent on a win:", smallestCost)
 	}
 }
-
-func (p *Player) buyItems() {
-	p.dmg += shop.weapons[4].damage
-	p.moneySpent += shop.weapons[4].cost
-	p.armor += shop.armors[0].armor
-	p.moneySpent += shop.armors[0].cost
-}
-
 func (p *Player) attack(b *Boss) {
 	dmg := p.dmg - b.armor
 	if dmg <= 0 {
