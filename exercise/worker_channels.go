d3de72902aaa05e121da4433fc2013f647ef1e85
Gergely Brautigam <skarlso777@gmail.com>
2016-01-22 06:39:05 +0100

    Added a mongodb connection exercise.

--- exercise/worker_channels.go
+++ exercise/worker_channels.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-func main() {
-	generatedPassword := make(chan int, 100)
-	correctPassword := make(chan int)
-	defer close(generatedPassword)
-	defer close(correctPassword)
-	go passwordIncrement(generatedPassword)
-	go checkPassword(generatedPassword, correctPassword)
-	pass := <-correctPassword
-	fmt.Println(pass)
-}
+// func main() {
+// 	generatedPassword := make(chan int, 100)
+// 	correctPassword := make(chan int)
+// 	defer close(generatedPassword)
+// 	defer close(correctPassword)
+// 	go passwordIncrement(generatedPassword)
+// 	go checkPassword(generatedPassword, correctPassword)
+// 	pass := <-correctPassword
+// 	fmt.Println(pass)
+// }
 
 func checkPassword(input <-chan int, output chan<- int) {
 	for {
